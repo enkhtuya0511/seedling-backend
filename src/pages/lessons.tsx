7f154ce5b360e74dc23a00a239668b65c076edf6
enkhtuya0511 <149783431+enkhtuya0511@users.noreply.github.com>
2024-08-12 04:48:24 +0800

    set up completed

--- src/pages/lessons.tsx
+++ src/pages/lessons.tsx
@@ -1,9 +1,21 @@
-const Page = () => {
+"use client";
+import { ReactElement } from "react";
+import type { NextPageWithLayout } from "./_app";
+import { useCoursesQuery } from "./generated";
+import Layout from "./login/layout";
+
+const Page: NextPageWithLayout = () => {
+  const { data, loading, error } = useCoursesQuery();
+  console.log("data", data);
   return (
     <div>
       <p>Courses page</p>
     </div>
   );
+};
+
+Page.getLayout = function getLayout(page: ReactElement) {
+  return <Layout>{page}</Layout>;
 };
 
 export default Page;
---
+++ src/provider/apollo-provider.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+interface Props extends React.PropsWithChildren {}
+
+const ApolloProviders = ({ children }: Props) => {
+  const client = new ApolloClient({
+    uri: "http://localhost:3000/api/graphql",
+    cache: new InMemoryCache(),
+  });
+
+  return <ApolloProvider client={client}>{children}</ApolloProvider>;
+};
+
+export default ApolloProviders;
